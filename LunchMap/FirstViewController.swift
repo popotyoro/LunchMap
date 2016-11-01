5273cf09cd638c97af8a01205bfbc58b41a7c4c2
popota <popota@popota-no-MacBook-Pro.local>
2016-11-01 23:12:12 +0900

    Initial Commit

---
+++ LunchMap/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  LunchMap
+//
+//  Created by popota on 2016/11/01.
+//  Copyright © 2016年 popota. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
