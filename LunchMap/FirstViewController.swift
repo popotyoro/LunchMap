eb4922f813a633703029029831dbfc42193bf1bc
popota <popota@popota-no-MacBook-Pro.local>
2016-11-03 13:40:33 +0900

    firstCommit

--- LunchMap/FirstViewController.swift
+++ LunchMap/FirstViewController.swift
@@ -7,18 +7,91 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    var locationManager: CLLocationManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // 現在地情報が使用できるかチェックする
+        startUpUserLocation()
+        
+        // 地図設定
+        setUpMapSettings()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    private func setUpMapSettings() {
+        
+        let coodinate = mapView.userLocation.coordinate
+        
+        let span = MKCoordinateSpanMake(0.01, 0.01)
+        
+        let region = MKCoordinateRegionMake(coodinate, span)
+        
+        mapView.setRegion(region, animated: true)
+        
+        mapView.setUserTrackingMode(.followWithHeading, animated: true)
+        
+        // 長押しのUIGestureRecognizerを生成
+        let longPressGesture: UILongPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(FirstViewController.recognizeLongPress(sender:)))
+        
+        mapView.addGestureRecognizer(longPressGesture)
+        
+    }
+    
+    private func startUpUserLocation() {
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.startUpdatingLocation()
+        }
+        
+        
+    }
+    
+    func recognizeLongPress(sender: UILongPressGestureRecognizer) {
+        
+        // 長押しの最中に何度もピンを生成しないようにする
+        if sender.state != .began {
+            return
+        }
+        
+        // 長押しした地点の座標を取得
+        let location:CGPoint = sender.location(in: mapView)
+        
+        // ピンを作成
+        let newPin = MKPointAnnotation()
+        
+        newPin.coordinate = mapView.convert(location, toCoordinateFrom: mapView)
+        newPin.title = "新しい地点"
+        
+        // 地図にピンをぶっ刺す
+        mapView.addAnnotation(newPin)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        default: break
+        }
+    }
+    
 
 
 }
